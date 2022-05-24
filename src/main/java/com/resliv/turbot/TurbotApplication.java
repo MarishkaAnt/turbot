e23bfaeed8f6cd6a96bb6bc39250b75bfb2939f4
Maria Antonova <marishka.ant@gmail.com>
2022-05-24 12:07:35 +0300

    Initial commit

---
+++ src/main/java/com/resliv/turbot/TurbotApplication.java
@@ -0,0 +1,13 @@
+package com.resliv.turbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resliv/turbot/TurbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resliv.turbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
