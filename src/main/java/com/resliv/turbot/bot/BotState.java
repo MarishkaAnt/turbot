eeee958594749dc553f2bea8948c327b386521ed
Mariia Antonova <mariia_antonova@epam.com>
2022-05-28 19:24:50 +0300

    add implementation of methods

--- src/main/java/com/resliv/turbot/bot/BotState.java
+++ src/main/java/com/resliv/turbot/bot/BotState.java
@@ -1,48 +1,84 @@
 package com.resliv.turbot.bot;
 
+import com.resliv.turbot.model.City;
+import com.resliv.turbot.model.User;
+import com.resliv.turbot.service.UserService;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public enum BotState {
 
-/*
     Start{
         @Override
         protected void enter(BotContext context) {
+            sendMessage(context, "Hello!");
+        }
 
+        @Override
+        public BotState nextState() {
+            return RequestName;
         }
     },
 
     RequestName{
         @Override
         protected void enter(BotContext context) {
+            sendMessage(context, "What is yor name?");
+        }
 
+        @Override
+        public void handleInput(BotContext context) {
+            context.getUser().setName(context.getInput());
+        }
+
+        @Override
+        public BotState nextState() {
+            return RequestCity;
         }
     },
 
     RequestCity{
         @Override
         protected void enter(BotContext context) {
+            sendMessage(context, "Enter the city name:");
+        }
 
+        @Override
+        public void handleInput(BotContext context) {
+            String input = context.getInput();
+            User user = context.getUser();
+        }
+
+        @Override
+        public BotState nextState() {
+            return InfoGiving;
         }
     },
 
     InfoGiving{
         @Override
         protected void enter(BotContext context) {
+            sendMessage(context, "Info:");
+        }
 
+        @Override
+        public BotState nextState() {
+            return Finish;
         }
     },
 
     Finish{
         @Override
         protected void enter(BotContext context) {
+            sendMessage(context, "Finish!");
+        }
 
+        @Override
+        public BotState nextState() {
+            return Start;
         }
     };
-*/
-/*
-
+    private UserService userService;
     private static BotState[] states = BotState.values();
     private boolean inputNeeded = false;
 
@@ -73,9 +109,9 @@ public enum BotState {
     }
 
     protected void sendMessage(BotContext context, String text) {
-        SendMessage message = new SendMessage()
-                .setChatId("1")
-                .setText(text);
+        SendMessage message = new SendMessage();
+        message.setChatId("1");
+        message.setText(text);
         try {
             context.getBot().execute(message);
         } catch (TelegramApiException e) {
@@ -93,7 +129,7 @@ public enum BotState {
     protected abstract void enter(BotContext context);
 
     public abstract BotState nextState();
-*/
+
 
 
 
