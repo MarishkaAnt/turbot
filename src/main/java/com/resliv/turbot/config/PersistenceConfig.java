7110f98179692b8d9abfb9ba654440307cf08c9a
Mariia Antonova <mariia_antonova@epam.com>
2022-05-28 19:27:17 +0300

    add persistence audit config

---
+++ src/main/java/com/resliv/turbot/config/PersistenceConfig.java
@@ -0,0 +1,9 @@
+package com.resliv.turbot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class PersistenceConfig {
+}
