1552df0e6ea75b02effc6966f4ab21913c72357c
Mariia Antonova <mariia_antonova@epam.com>
2022-05-28 15:38:32 +0300

    add bot registration

--- src/main/java/com/resliv/turbot/TurbotApplication.java
+++ src/main/java/com/resliv/turbot/TurbotApplication.java
@@ -1,13 +1,26 @@
 package com.resliv.turbot;
 
+import com.resliv.turbot.bot.Bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class TurbotApplication {
 
+	private static final String BOT_NAME = "bot_TurBot";
+	private static  final String BOT_TOKEN = "5313838550:AAEg87Zn9mwEInZeealhynD3hfc1EUJF1ZM";
+
 	public static void main(String[] args) {
 		SpringApplication.run(TurbotApplication.class, args);
+		try {
+			TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+			botsApi.registerBot(new Bot(BOT_NAME, BOT_TOKEN));
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
