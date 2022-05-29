b990729136bd8237e08ed6eb0fb23ecc996aeb9c
Mariia Antonova <mariia_antonova@epam.com>
2022-05-29 13:45:20 +0300

    add default values to table state_messages

--- src/main/java/com/resliv/turbot/model/StateMessage.java
+++ src/main/java/com/resliv/turbot/model/StateMessage.java
@@ -2,22 +2,21 @@ package com.resliv.turbot.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "state_messages")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Data
 public class StateMessage {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String state;
     private String message;
